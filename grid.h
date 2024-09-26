a036d5cdce03d92b9406df3b0e045c62124e1104
Salahe-eddine BOUHDJEUR <salahe-eddine.bouhdjeur@horizontrading.io>
2024-09-27 01:46:19 +0200

    delete .exe

--- grid.h
+++
@@ -1,7 +0,0 @@
-#define ROW 12
-#define COLUMN 15
-
-int **createGrid(int row, int column);
-char **createCharGrid(int row, int column, int **grid);
-void displayGrid(int row, int column, int **grid);
-void addPiece(int **grid, int column, int playerCross);
