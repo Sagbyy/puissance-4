419319576eefde31aad184ccde937e9f67758e1d
Salahe-eddine BOUHDJEUR <salahe-eddine.bouhdjeur@horizontrading.io>
2024-09-27 01:46:34 +0200

    delete .exe

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "grid.c"
+#include "player.c"
+
+int main(void) {
+    int playerCross = 1;
+    int **grid = createGrid(ROW, COLUMN);
+
+    while (1)
+    {
+        displayGrid(ROW, COLUMN, grid);
+        int pieceCol = playerPlay(playerCross);
+        addPiece(grid, pieceCol - 1, playerCross);
+
+        if (playerCross == 1)
+        {
+            playerCross = 2;
+        }
+        else {
+            playerCross = 1;
+        }
+        
+        system("cls");
+    }
+    
+    return 0;
+}
---
+++ player.c
@@ -0,0 +1,17 @@
+#include "player.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int playerPlay(int playerCross) {
+    if (playerCross == 1) {
+        printf("Au tour de ❌\n");
+    } else {
+        printf("Au tour de 🟢\n");
+    }
+
+    printf("Entrez la colonne: ");
+    int column;
+    scanf("%d", &column);
+
+    return column;
+}
